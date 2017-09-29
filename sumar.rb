a585adac0a178a83b7e2a7767c88d2efc0eac124
Domingo Castañeda <sedeo95@gmail.com>
2017-09-29 20:03:08 +0200

    Cambia el mensaje que pide el segundo numero

--- sumar.rb
+++ sumar.rb
@@ -1,5 +1,5 @@
 print "Introduce el primer numero: "
 n1 = gets.chomp.to_i
-print "Introduce el segundo numero: "
+print "Introduce el numero que viene luego: "
 n2 = gets.chomp.to_i
 puts "La suma vale #{n1 + n2}"
