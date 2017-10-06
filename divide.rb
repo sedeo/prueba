b0c18d11af9bbb3bce73e1c0a8d7b381436e156c
Domingo Castañeda <sedeo95@gmail.com>
2017-10-06 20:12:10 +0200

    Corregido divide.rb

--- divide.rb
+++ divide.rb
@@ -1,5 +1,5 @@
 print "Introduce el primer numero: "
-n1 = gets.chomp
+n1 = gets.chomp.to_i
 print "Introduce el segundo numero: "
-n2 = gets.chomp
+n2 = gets.chomp.to_i
 puts "El resultado es #{n1 / n2}."
